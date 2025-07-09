194aa754ed41874e15ddb5953feb2839d580de49
Ensar Akbaş <ensarr.akbas@gmail.com>
2025-07-09 16:53:55 +0300

    feat: putText added on RotatinExampleEnsar.py

--- src/executors/RotationExampleEnsar.py
+++ src/executors/RotationExampleEnsar.py
@@ -34,10 +34,20 @@ class RotationExampleEnsar(Component):
     def gray(self, img):
         return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    def write(self, img):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        org = (100, 150)
+        fontScale = 4
+        color = (255, 0, 0)
+        thickness = 4
+        return cv2.putText(img, 'Ensar Deneme', org, font,
+                            fontScale, color, thickness, cv2.LINE_AA)
+
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
         img.value = self.rotation(img.value)
         img.value = self.gray(img.value)
+        img.value = self.write(img.value)
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
         packageModel = build_response(context=self)
         return packageModel
